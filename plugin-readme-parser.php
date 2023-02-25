6b721a716512b5d33d54c77a97df1cd66f8b52ff
Caity Ross <internet@pandammonium.org>
2023-02-25 15:48:16 +0000

    Adds constant for name of plugin.

--- plugin-readme-parser.php
+++ plugin-readme-parser.php
@@ -25,6 +25,7 @@
 
 define( 'pandammonium_readme_parser_version', '2.0.0' );
 define( 'pandammonium_readme_parser_filename', 'plugin-readme-parser' );
+define( 'pandammonium_readme_parser_name', 'Plugin-readme Parser' );
 
 /**
 * Main Includes
